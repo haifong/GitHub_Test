382078ed3bad72801b41abc037dfeb27c0ff792b
NighTnT <67869218+haifong@users.noreply.github.com>
2024-09-09 15:25:30 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,31 +1,50 @@
-node("JenkinsServer")
+def is_online(node_name)
 {
-    def tasks = [:]
+    def b_ret = false
     def Jenkins_inst = Jenkins.get()
     if(Jenkins_inst != null)
     {
+        def node_inst = Jenkins_inst.getNode(node_name)
+        if(node_inst != null && node_inst.getComputer().isOnline())
+        {
+            b_ret = true
+        }
+        else
+        {
+            b_ret = false
+        }
+    }
+    {
+        b_ret = false
+    }
+
+    return b_ret
+}
+
+node("JenkinsServer")
+{
+    def tasks = [:]
+    if(is_online"Win10"())
+    {
+        print("Node Win10 Exist")
         tasks["Win10 Test"] = 
         {
-            def node_inst = Jenkins_inst.getNode("Win10")
-            if(node_inst != null && node_inst.getComputer().isOnline())
-            {
-                print("Node Win10 Exist")
-                build job:'agent_test', parameters:[
+            build job:'agent_test', parameters:[
                     string(name: 'TARGET_NODE_NAME', value:'Win10')
                 ]
-            }
-            else
-            {
-                print("Node Win10 not Exist")
-            }
         }
-        //try 
-        {
-            parallel tasks
-        }
-        //catch(exception)
-        {
-            //print("run tasks fail")
-        }
+    }
+    else
+    {
+        print("Node Win10 not Exist")
+    }
+   
+    //try 
+    {
+        parallel tasks
+    }
+    //catch(exception)
+    {
+        //print("run tasks fail")
     }
 }
