db5b2621fdb716c419dace32d93dfc9a202372bf
NighTnT <67869218+haifong@users.noreply.github.com>
2024-09-09 14:46:28 +0800

    Update agent.groovy hello

--- agent.groovy
+++ agent.groovy
@@ -3,7 +3,8 @@ node("Win10")
     stage("run py")
     {
         checkout scm
-        bat 'python ./hello.py'
+        //bat 'python ./hello.py'
+        bat 'echo hello'
         bat 'echo %errorlevel%'
     }
 }
