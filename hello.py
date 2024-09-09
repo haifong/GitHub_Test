4cdf6a69e8edd1c9a649ae7b4308d16e02b1c247
NighTnT <67869218+haifong@users.noreply.github.com>
2024-09-09 15:35:04 +0800

    Update hello.py write file

--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
+import os
+import datetime
+
+current_path = os.getcwd()
+
+current_time = datetime.datetime.now().strftime("%H%M%S")
+
+file_name = os.path.join(current_path, f"{current_time}.txt")
+
+with open(file_name, 'w') as file:
+    file.write("This file is created based on the current time.")
+
+print(f"File '{file_name}' created successfully in the directory {current_path}.")
+
+
+
 print("Hello!!!")
-exit(1)
+#exit(1)
