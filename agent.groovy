3273a8b0f3fe18bb61d234f4edbe388999ce4bff
NighTnT <67869218+haifong@users.noreply.github.com>
2024-09-09 15:08:00 +0800

    Update agent.groovy

--- agent.groovy
+++ agent.groovy
@@ -3,10 +3,10 @@ node("Win10")
     stage("run py")
     {
         checkout scm
-        //bat 'python ./hello.py'
+        bat 'python hello.py'
         //bat 'python --version'
         bat 'echo %errorlevel%'
-        def output = bat(script: 'python hello.py', returnStdout: true).trim()
-        echo "Python script output: ${output}"
+        //def output = bat(script: 'python hello.py', returnStdout: true).trim()
+        //echo "Python script output: ${output}"
     }
 }
